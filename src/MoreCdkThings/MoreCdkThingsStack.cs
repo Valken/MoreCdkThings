43aa8968d250778348771958539275a6162b4abf
Neil Mackey <neilmackey@gmail.com>
2024-07-27 18:03:55 +0100

    Initial commit

---
+++ src/MoreCdkThings/MoreCdkThingsStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace MoreCdkThings
+{
+    public class MoreCdkThingsStack : Stack
+    {
+        internal MoreCdkThingsStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
